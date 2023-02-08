915085407faca48f0dc84cb20f70ce5f9c26005d
Martin Cepeda <26345924+cepedus@users.noreply.github.com>
2023-02-08 22:03:17 +0100

    proper gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   site: "https://cepedus.xyz/",
@@ -19,6 +20,12 @@ export default defineConfig({
     react(),
     sitemap(),
     mdx(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
   ],
   markdown: {
     remarkPlugins: [
